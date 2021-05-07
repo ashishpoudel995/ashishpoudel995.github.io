2639c52b6f0fa180eb531f895e9174f4ed7a3d88
Ashish Poudel <ashishpoudel995@gmail.com>
2021-05-07 13:19:00 +0545

    Website Updated

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Logo from './images/logo.png';
+import Resume from './files/Resume.pdf';
+
 class NavigationBar extends React.Component{
     render(){
         return(
@@ -8,6 +10,7 @@ class NavigationBar extends React.Component{
                     <a class="navbar-brand" href="#"><img src={Logo}/></a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
+                      <span className="icon-bar"></span>
                       <span className="icon-bar"></span>
                       <span className="icon-bar"></span>
                       <span className="icon-bar"></span>
@@ -20,8 +23,11 @@ class NavigationBar extends React.Component{
                         <li class="nav-item">
                           <a class="nav-link" href="#Projects">Projects</a>
                         </li>
+                        <li className="nav-item">
+                          <a className="nav-link" href={Resume} target="_new"><i class="far fa-file-pdf"></i>&nbsp;Resume</a>
+                        </li>
                         <li class="nav-item">
-                          <a class="nav-link" href="#ContactUs">Say Hello</a>
+                          <a class="nav-link" href="#ContactUs">Say Hello&nbsp;<i class="material-icons">waving_hand</i></a>
                         </li>
                       </ul>
                     </div>
