a8f34cb9809540a8b5bdd296264dbf95ef644cd3
Ashish Poudel <ashishpoudel995@gmail.com>
2021-07-07 15:32:10 +0545

    Projects and Resume Updated

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
-import React,{useEffect} from 'react';
-import ReactDOM from 'react-dom';
-import Intro from './Intro';
-import NavigationBar from './NavigationBar';
-import Projects from './Projects/Projects';
-import {Projectlists} from './Projects/Projectlists';
-import ContactUs from './ContactUs';
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import Intro from "./Intro";
+import NavigationBar from "./NavigationBar";
+import Projects from "./Projects/Projects";
+import { Projectlists } from "./Projects/Projectlists";
+import ContactUs from "./ContactUs";
 
-const App=()=>{
-    return(
-        <div>
-            <NavigationBar/>
-            <Intro/>
-            <Projects Projectlists={Projectlists}/>
-            <ContactUs/>
-        </div>
-    )
-}
+const App = () => {
+  return (
+    <div>
+      <NavigationBar />
+      <Intro />
+      <Projects Projectlists={Projectlists} />
+      <ContactUs />
+    </div>
+  );
+};
 
-ReactDOM.render(
-    <App/>,
-    document.querySelector("#root")
-)
+ReactDOM.render(<App />, document.querySelector("#root"));
